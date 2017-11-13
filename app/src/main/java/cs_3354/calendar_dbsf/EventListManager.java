fbf58b108c61b945eb81485f0931ef4b4ac6324e
BobsCandyCanes <grantmatthews28@yahoo.com>
2017-11-12 18:46:22 -0600

    Created method signatures

---
+++ app/src/main/java/cs_3354/calendar_dbsf/EventListManager.java
@@ -0,0 +1,74 @@
+package cs_3354.calendar_dbsf;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * Created by Grant M on 11/6/2017.
+ */
+
+public class EventListManager
+{
+    private ArrayList<Event> events;
+
+    /**
+     * Adds an Event to the list of events
+     * @param e the Event to be added
+     */
+    public void addEvent(Event e)
+    {
+        //TODO write method
+    }
+
+    /**
+     * Removes an Event from the list of events
+     * @param e the Event to be removed
+     * @throws NullPointerException if the event is not found in the list
+     */
+    public void removeEvent(Event e) throws NullPointerException
+    {
+        //TODO write method
+    }
+
+    /**
+     * Takes in two dates and returns a list of any events that occur between the two dates
+     * @param start the starting date
+     * @param end the ending date
+     * @return an array of Events that occur between the start and end date
+     */
+    public Event[] getEventsBetween(Date start, Date end)
+    {
+        //TODO write method
+        return null;
+    }
+
+    /**
+     * Edits an event that already exists
+     * @param e the Event that will be edited
+     * @param d the new Date of the event
+     * @param sTime the new start time of the event
+     * @param eTime the new end time of the event
+     * @param alarms a list of alarms associated with the event
+     * @param eType the type of the event
+     */
+    public void editEvent(Event e,
+                             Date d,
+                             int sTime,
+                             int eTime,
+                             ArrayList<Alarm> alarms,
+                             String eType)
+    {
+        //TODO write method
+    }
+
+    /**
+     * Returns true if the given event overlaps with other events
+     * @param e the event being checked
+     * @return true if there are time conflicts, otherwise returns false
+     */
+    public boolean checkTimeConflicts(Event e)
+    {
+        //TODO write method
+        return false;
+    }
+}
