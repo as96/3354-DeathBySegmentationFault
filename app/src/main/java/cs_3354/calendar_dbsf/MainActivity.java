695f04aca9a0b60ddc3c16cc9cd83f9df3cfa7b5
juan <jmflorezff@gmail.com>
2017-11-22 15:58:10 -0600

    Creates an alarm 30 seconds in the future when starting the application

--- app/src/main/java/cs_3354/calendar_dbsf/MainActivity.java
+++ app/src/main/java/cs_3354/calendar_dbsf/MainActivity.java
@@ -2,6 +2,11 @@ package cs_3354.calendar_dbsf;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GregorianCalendar c = new GregorianCalendar();
+        c.setTimeInMillis(new Date().getTime() + 30000);
+        new Alarm(c, this);
     }
 }
