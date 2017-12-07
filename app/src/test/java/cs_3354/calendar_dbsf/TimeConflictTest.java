67789fdf817866259eea06ed89ff61b8f6ddad2d
hl07 <h78717@gmail.com>
2017-12-06 23:52:19 -0600

    test

--- app/src/test/java/cs_3354/calendar_dbsf/TimeConflictTest.java
+++ app/src/test/java/cs_3354/calendar_dbsf/TimeConflictTest.java
@@ -1,13 +1,32 @@
 package cs_3354.calendar_dbsf;
 
+import org.junit.Before;
 import org.junit.Test;
 
 /**
  * Created by Lee on 12/6/17.
  */
 
-public class TimeConflictTest {
-    @Test
+public class TimeConflictTest
+{
+    EventListManager test1;
+    EventListManager test2;
+    private Event e;
 
-    
+    @Before
+    public void initialize()
+    {
+        test1 = EventListManager.getInstance();
+        test2 = EventListManager.getInstance();
+    }
+
+    @Test
+    public void testConflicts()
+    {
+        test1.addEvent(e);
+        test2.addEvent(e);
+
+    }
+
+
 }
